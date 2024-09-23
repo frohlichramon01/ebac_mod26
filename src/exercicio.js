4e26aa002beaa4eb41348787b5a05e6ba9001089
frohlichramon01 <frohlichramon01@gmail.com>
2024-09-22 23:46:40 -0300

    exercicio feito

---
+++ src/exercicio.js
@@ -0,0 +1,13 @@
+"use strict";
+// uma função de multiplicação que deverá receber como argumentos dois números e retornar a multiplicação deles
+function multiplicacaoDeDoisFatores(fator1, fator2) {
+    return (fator1 * fator2);
+}
+;
+console.log(multiplicacaoDeDoisFatores(10, 5));
+//uma função de saudação que deverá receber um nome e retornar a concatenação “Olá “ + nome;
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+;
+console.log(saudacao("Ramon"));
---
+++ src/exercicio.ts
@@ -0,0 +1,15 @@
+// uma função de multiplicação que deverá receber como argumentos dois números e retornar a multiplicação deles
+function multiplicacaoDeDoisFatores (fator1: number, fator2: number): number {
+    return (fator1 * fator2);
+};
+
+console.log(multiplicacaoDeDoisFatores(10, 5));
+
+
+
+//uma função de saudação que deverá receber um nome e retornar a concatenação “Olá “ + nome;
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;
+};
+
+console.log(saudacao("Ramon"));
